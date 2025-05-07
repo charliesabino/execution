a3182da0f4286144c2c3d10253c6bd28bb85110a
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-05-07 14:57:39 -0500

    Add then_receiver

--- toy/toy.hpp
+++ toy/toy.hpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <functional>
 #include <tuple>
 #include <type_traits>
 #include <utility>
@@ -35,4 +36,17 @@ template <typename... Ts> auto just(Ts &&...args) {
   return just_sender<std::decay_t<Ts>...>{std::forward<Ts>(args)...};
 }
 
+template <typename Receiver, typename Function> class then_receiver {
+public:
+  then_receiver(Receiver receiver, Function function)
+      : out_receiver_{std::move(receiver)}, function_{std::move(function)} {}
+
+  template <typename... Ts> auto set_value(Ts &&...args) -> void {
+    out_receiver_.set_value(std::invoke(function_, std::forward<Ts>(args)...));
+  }
+
+private:
+  Receiver out_receiver_;
+  Function function_;
+};
 } // namespace toy
