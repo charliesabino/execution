0d4b31b6d898f537c43b24e7c952d789d57e1dd2
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-05-12 15:49:43 -0500

    Begin thread pool

---
+++ toy/thread_pool.hpp
@@ -0,0 +1,52 @@
+#include <condition_variable>
+#include <cstdint>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
+namespace toy {
+
+class thread_pool {
+  std::mutex mtx_;
+  std::condition_variable cv_;
+  std::queue<std::function<void()>> tasks_;
+  std::vector<std::thread> workers_;
+  bool done_ = false;
+
+  auto run() -> void {
+    std::function<void()> task{};
+    {
+      std::unique_lock guard(mtx_);
+      cv_.wait(guard,
+               [this]() { return this->done_ || !this->tasks_.empty(); });
+      if (done_ || tasks_.empty()) {
+        return;
+      }
+      auto task = tasks_.front();
+      tasks_.pop();
+    }
+    task();
+  }
+
+public:
+  thread_pool(auto num_workers = std::thread::hardware_concurrency()) {
+    while (num_workers--) {
+      workers_.emplace_back([this]() { run(); });
+    }
+  }
+
+  // Notably, this enables currently running tasks to continue running, even
+  // after the thread pool exits scope. This may raise the concern of dangling
+  // pointers—the worker threads contain a pointer to the thread pool, which may
+  // expire before the thread exits. However, the code ensures that this pointer
+  // is never accessed.
+  ~thread_pool() {
+    std::unique_lock guard(mtx_);
+    done_ = false;
+    for (auto &worker : workers_) {
+      worker.join();
+    }
+  }
+};
+}; // namespace toy
