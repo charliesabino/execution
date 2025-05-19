12fb775a3eba8abddbb43db95f89995d0abe2ab1
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-05-19 16:55:55 -0500

    Remove execution folder

--- include/execution/execution.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <myexecution/concepts.hpp>
-
-#include <myexecution/sender/just.hpp>
-#include <myexecution/sender/then.hpp>
-
-#include <myexecution/schedulers/inline_scheduler.hpp>
-#include <myexecution/schedulers/thread_pool_scheduler.hpp>
