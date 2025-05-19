2b00d96de65604b601a5bfac05e7ecf7ad4f3a7a
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-05-19 17:05:59 -0500

    Split thread pool out into source and header

--- include/myexecution/schedulers/thread_pool_scheduler.hpp
+++ include/myexecution/schedulers/thread_pool_scheduler.hpp
@@ -17,52 +17,13 @@ private:
   std::vector<std::thread> workers_;
   bool done_ = false;
 
-  auto run() -> void {
-    for (;;) {
-      std::function<void()> task{};
-      {
-        std::unique_lock guard(mtx_);
-        cv_.wait(guard, [this]() { return done_ || !tasks_.empty(); });
-        if (done_ || tasks_.empty()) {
-          return;
-        }
-        task = tasks_.front();
-        tasks_.pop();
-      }
-      task();
-    }
-  }
+  auto run() -> void;
 
 public:
-  explicit thread_pool(auto num_workers = std::thread::hardware_concurrency()) {
-    while (num_workers--) {
-      workers_.emplace_back([this]() { run(); });
-    }
-  }
+  explicit thread_pool(auto num_workers = std::thread::hardware_concurrency());
+  ~thread_pool();
 
-  // Notably, this enables currently running tasks to continue running, even
-  // after the thread pool exits scope. This may raise the concern of dangling
-  // references—the worker threads contain a reference to the thread pool, which
-  // may expire before the thread exits. However, the code ensures that this
-  // reference is never accessed. Thus, we maintain our invariant.
-  ~thread_pool() {
-    {
-      std::lock_guard guard(mtx_);
-      done_ = true;
-    }
-    cv_.notify_all();
-    for (auto &worker : workers_) {
-      worker.join();
-    }
-  }
-
-  auto post(std::function<void()> task) -> void {
-    {
-      std::lock_guard guard(mtx_);
-      tasks_.emplace(std::move(task));
-    }
-    cv_.notify_one();
-  };
+  auto post(std::function<void()> task) -> void;
 
 public:
   class scheduler {
@@ -102,13 +63,6 @@ public:
     }
   };
 
-  // "deducing this" wasn't not convered in class, but I have seen
-  // it in conference talks and wanted to practice using it. Doing so eliminates
-  // the unecessary space overhead of more pointer copies (although the compiler
-  // might optimize them away anyways). Generally, I felt that sticking to
-  // references over pointers was a good idea.
-  auto get_scheduler(this thread_pool &self) -> scheduler {
-    return scheduler{self};
-  }
+  auto get_scheduler(this thread_pool &self) -> scheduler;
 };
 }; // namespace execution
