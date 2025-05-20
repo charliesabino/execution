e63d8a09ce096c09741d71bf529183ee1d545f55
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-05-20 11:29:22 -0500

    Improve example documentation

--- examples/quickstart.cpp
+++ examples/quickstart.cpp
@@ -1,3 +1,4 @@
+#include "myexecution/concepts.hpp"
 #include <myexecution/myexecution.hpp>
 
 #include <iostream>
@@ -18,18 +19,24 @@ int main() {
 
   // 1.  Inline scheduler – runs on the caller’s thread
   auto inl_sched = inline_scheduler{};
-  auto inl_sender =
+  sender auto inl_sender =
       inl_sched.schedule() | then([] { return "hello from inline"; });
-  inl_sender.connect(print_receiver{}).start();
+  operation_state auto inl_op = inl_sender.connect(print_receiver{});
+  inl_op.start();
+
+  // note: you could do also just do
+  // inl_sender.connect(print_receiver{}).start(); I stored the operation state
+  // for illustrative purposes only. The same applies to the example below
 
   // 2.  Thread‑pool scheduler – runs on a worker thread
   thread_pool pool(std::thread::hardware_concurrency());
   auto pool_sched = pool.get_scheduler();
 
-  auto pool_sender = pool_sched.schedule() | then([] { return 21; }) |
-                     then([](int v) { return v * 2; });
+  sender auto pool_sender = pool_sched.schedule() | then([] { return 21; }) |
+                            then([](int v) { return v * 2; });
 
-  pool_sender.connect(print_receiver{}).start();
+  operation_state auto pool_op = pool_sender.connect(print_receiver{});
+  pool_op.start();
 
   // give the pool a moment to finish before main exits
   // sync wait gave me too much trouble to implement :(
