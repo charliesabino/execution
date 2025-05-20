a8b235dab9786422c45423d5dac4cd162d85e05a
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-05-20 10:36:19 -0500

    Add comment

--- examples/quickstart.cpp
+++ examples/quickstart.cpp
@@ -32,5 +32,6 @@ int main() {
   pool_sender.connect(print_receiver{}).start();
 
   // give the pool a moment to finish before main exits
+  // sync wait gave me too much trouble to implement :(
   std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
